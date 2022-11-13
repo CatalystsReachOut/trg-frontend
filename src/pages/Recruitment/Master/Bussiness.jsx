44ac2af7084084ebfe10485e64d3774b2fd0615a
prkskrs <prkskrs@gmail.com>
2022-11-13 17:56:56 +0530

    business integrated

--- src/pages/Recruitment/Master/Bussiness.jsx
+++ src/pages/Recruitment/Master/Bussiness.jsx
@@ -9,96 +9,171 @@ import { Sorter } from '../../../helpers/Sorter'
 import TextArea from './../../../components/Input/TextArea'
 import * as apiProvider from '../../../services/api/recruitment'
 
-const Bussiness = () => {
-  const [name, setName] = useState()
+const Bussiness = ({ notify, enterLoading, exitLoading, loadings }) => {
+  const [business, setBusiness] = useState({
+    businessName:"",
+    address:"",
+    businessUrl:"",
+    businessCode:"",
+    businessLogo:"",
+    summary:"",
+    description:""
+  })
 
 
   const columns = [
     {
-      title: "Round",
-      dataIndex: "name"
+      title: "Business Name",
+      dataIndex: "businessName",
+      sorter: {
+        compare: Sorter.DEFAULT,
+        multiple: 2
+      }
     },
+    {
+      title: "Address",
+      dataIndex: "address",
+      sorter: {
+        compare: Sorter.DEFAULT,
+        multiple: 1
+      }
+    },
+    {
+      title: "Business Logo",
+      dataIndex: "businessLogo",
+      sorter: {
+        compare: Sorter.DEFAULT,
+        multiple: 1
+      }
+    },
+    {
+      title: "Business URL",
+      dataIndex: "businessUrl",
+      sorter: {
+        compare: Sorter.DEFAULT,
+        multiple: 1
+      }
+    },
+    {
+      title: "Business Code",
+      dataIndex: "businessCode",
+      sorter: {
+        compare: Sorter.DEFAULT,
+        multiple: 1
+      }
+    },
+    {
+      title: "Summary",
+      dataIndex: "summary",
+      sorter: {
+        compare: Sorter.DEFAULT,
+        multiple: 1
+      }
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      sorter: {
+        compare: Sorter.DEFAULT,
+        multiple: 1
+      }
+    },
+    
     
   ];
 
-  const data = [
-    {
-      key: "1",
-      name: "John Brown",
-      chinese: 98,
-      math: 60,
-      english: 70,
-      action:<Action/>
-    },
-    {
-      key: "2",
-      name: "Jim Green",
-      chinese: 98,
-      math: 66,
-      english: 89,
-      action:<Action/>
-    },
-    {
-      key: "3",
-      name: "Joe Black",
-      chinese: 98,
-      math: 90,
-      english: 70,
-      action:<Action/>
-    },
-    {
-      key: "4",
-      name: "Jim Red",
-      chinese: 88,
-      math: 99,
-      english: 89,
-      action:<Action/>
-    }
-  ];
-
-  const getData =()=>{
-    apiProvider.getBusiness()
-    .then(res=>{
-      console.log(res)
-    })
-    .catch(err=>{
-      console.log(err)
-    })
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setBusiness(prev=>({
+      ...prev,
+      [name]:value
+    }))
   }
 
-  const handleSubmit =()=>{
-    apiProvider.createBusiness({name:name})
-    .then(res=>{
-      console.log(res)
-    })
-    .catch(err=>{
-      console.log(err)
+  const [data,setData] = useState([]);
+  const [profileData, setProfileData] = useState([])
+
+
+  const getData = () => {
+    enterLoading(2)
+    apiProvider.getBusiness()
+        .then(res => {
+  
+            if (res.isSuccess) {
+              setData(res.data)
+                const arr = res.data.map(data => ({
+                    value: data._id,
+                    label: data.businessName
+                }))
+                setProfileData(arr)
+            }
+            return exitLoading(2)
+        })
+        .catch(err => {
+            console.log(err)
+            return exitLoading(2)
+  
+        })
+  }
+  
+  const handleSubmit = () => {
+  
+    enterLoading(1)
+    return apiProvider.createBusiness(business)
+        .then(res => {
+            exitLoading(1)
+            if (res.isSuccess) {
+                clearData()
+                getData()
+                return notify('success', 'added success');
+            } else {
+                return notify('error', res.message);
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            exitLoading(1)
+            return notify('error', err.message);
+        })
+  }
+  
+  const clearData = () => {
+    setBusiness({
+      businessName:"",
+      address:"",
+      businessUrl:"",
+      businessCode:"",
+      businessLogo:"",
+      summary:"",
+      description:""
     })
   }
   
-  useEffect(()=>{
+  useEffect(() => {
     getData();
-  },[])
+  }, [])
 
   return (
     <div>
         <Card>
-          <div className='font-bold'> Add Round </div>
+          <div className='font-bold'> Add Business </div>
           <div className='grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 mt-4'>
             <div className="col-span-1">
               <Input
               label={'Title'}
               placeHolder = {'Enter Title'}
-              value = {name}
-              onChange = {e => setName(e.target.value)}
+              value = {business?.businessName}
+              name="businessName"
+              onChange = {handleChange}
               />
             </div>
             <div className="col-span-1">
               <Input
               label={'Address'}
               placeHolder = {'Enter Address'}
-              value = {name}
-              onChange = {e => setName(e.target.value)}
+              value = {business?.address}
+              name="address"
+              onChange = {handleChange}
               />
             </div>
             <div className="col-span-1">
@@ -106,46 +181,51 @@ const Bussiness = () => {
               label={'Bussiness Logo'}
               placeHolder = {'Bussiness Logo'}
               type="file"
-              value = {name}
-              onChange = {e => setName(e.target.value)}
+              value = {business?.businessLogo}
+              name = "businessLogo"
+              onChange = {handleChange}
               />
             </div>
             <div className="col-span-1">
               <Input
               label={'Bussiness URL'}
               placeHolder = {'Bussiness URL'}
-              value = {name}
-              onChange = {e => setName(e.target.value)}
+              value = {business?.businessUrl}
+              name="businessUrl"
+              onChange = {handleChange}
               />
             </div>
             <div className="col-span-1">
               <Input
               label={'Bussiness Code'}
               placeHolder = {'Bussiness Code'}
-              value = {name}
-              onChange = {e => setName(e.target.value)}
+              value = {business?.businessCode}
+              name="businessCode"
+              onChange = {handleChange}
               />
             </div>
             <div className="col-span-1">
               <Input
               label={'Summary'}
               placeHolder = {'Summary'}
-              value = {name}
-              onChange = {e => setName(e.target.value)}
+              value = {business?.summary}
+              name="summary"
+              onChange = {handleChange}
               />
             </div>
             <div className="col-span-1">
               <TextArea
               label={'Description'}
               placeHolder = {'Description'}
-              value = {name}
-              onChange = {e => setName(e.target.value)}
+              value = {business?.description}
+              name="description"
+              onChange = {handleChange}
               />
             </div>
           </div>
           <div className="flex justify-end mt-3">
             <Button 
-            title="Add Round" 
+            title="Add Business" 
             className={'min-w-[100px]'}
             onClick={handleSubmit}
             />
